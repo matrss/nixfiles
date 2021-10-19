6b7414961fbaf5bac327fd5c9f8fdf4c943a0784
Matthias Riße <matthias.risze@t-online.de>
2021-10-19 21:24:42 +0200

    Build nixosConfigurations in CI and push to cache

--- hosts/ara.nix
+++ hosts/ara.nix
@@ -198,8 +198,10 @@ in
   # Serve nix store via ssh
   nix.sshServe = {
     enable = true;
-    keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBGFjDIcea7ScFIv87nevx9ShfPGEoWedQ6JNRoqVPLg root@andromeda" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeFg7nYVjzT1v6umYLpJaFfvvHqyxiixbWOX7EaOn6Y root@ara" ];
+    keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWXOgpAWXNPVnq9pw3qmtUPl+yynT9GCmG0JxWPsJt4 gitlab-ci" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBGFjDIcea7ScFIv87nevx9ShfPGEoWedQ6JNRoqVPLg root@andromeda" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeFg7nYVjzT1v6umYLpJaFfvvHqyxiixbWOX7EaOn6Y root@ara" ];
   };
+
+  nix.trustedUsers = [ "nix-ssh" ];
 
   # services.f2b-miner = {
   #   enable = true;
