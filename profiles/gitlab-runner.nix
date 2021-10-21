d10bf05d322c9aab080fecad4ccfc4c885d49121
Matthias Riße <matthias.risze@t-online.de>
2021-10-21 09:32:38 +0200

    Use normal docker gitlab runners

--- profiles/gitlab-runner.nix
+++ profiles/gitlab-runner.nix
@@ -7,51 +7,20 @@
   virtualisation.docker.enable = true;
   services.gitlab-runner = {
     enable = true;
-    concurrent = 1;
     services =
       let
-        nixFlakesRunner = with lib; {
+        dockerRunner = with lib; {
           # File should contain at least these two variables:
           # `CI_SERVER_URL`
           # `REGISTRATION_TOKEN`
           registrationConfigFile = config.sops.secrets.gitlab-runner-env.path;
-          registrationFlags = [ "--locked=false" ];
-          dockerImage = "alpine";
-          dockerVolumes = [
-            "/nix/store:/nix/store:ro"
-            "/nix/var/nix/db:/nix/var/nix/db:ro"
-            "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
-          ];
-          dockerDisableCache = true;
-          preBuildScript = pkgs.writeScript "setup-container" ''
-            mkdir -p -m 0755 /nix/var/log/nix/drvs
-            mkdir -p -m 0755 /nix/var/nix/gcroots
-            mkdir -p -m 0755 /nix/var/nix/profiles
-            mkdir -p -m 0755 /nix/var/nix/temproots
-            mkdir -p -m 0755 /nix/var/nix/userpool
-            mkdir -p -m 1777 /nix/var/nix/gcroots/per-user
-            mkdir -p -m 1777 /nix/var/nix/profiles/per-user
-            mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
-            mkdir -p -m 0700 "$HOME/.nix-defexpr"
-            mkdir -p -m 0755 /etc/nix
-            echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
-            . ${pkgs.nixFlakes}/etc/profile.d/nix.sh
-            ${pkgs.nixFlakes}/bin/nix-env -i ${concatStringsSep " " (with pkgs; [ nixFlakes cacert git openssh ])}
-          '';
-          environmentVariables = {
-            ENV = "/etc/profile";
-            USER = "root";
-            NIX_REMOTE = "daemon";
-            PATH = "/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin";
-            NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
-          };
-          tagList = [ "nixFlakes" ];
+          dockerImage = "debian:stable";
         }; in
       {
         # runners for building in docker via host's nix-daemon
         # nix store will be readable in runner, might be insecure
-        nix = nixFlakesRunner // { protected = false; registrationFlags = nixFlakesRunner.registrationFlags ++ [ "--name=nix_ara" ]; };
-        nixProtected = nixFlakesRunner // { protected = true; registrationFlags = nixFlakesRunner.registrationFlags ++ [ "--name=nixProtected_ara" ]; };
+        docker = lib.recursiveUpdate dockerRunner { protected = false; registrationFlags = [ "--name=ara-docker" ]; };
+        dockerProtected = lib.recursiveUpdate dockerRunner { protected = true; registrationFlags = [ "--name=ara-docker-protected" ]; };
       };
   };
 }
