5d27638fb1496280da3afbe399089ce3536f2fbc
Matthias Riße <matthias.risze@t-online.de>
2021-11-15 19:53:01 +0100

    Remove obsolete nix-direnv enableFlakes option

--- users/profiles/direnv/default.nix
+++ users/profiles/direnv/default.nix
@@ -3,9 +3,6 @@
     enable = true;
     stdlib = builtins.readFile ./direnvrc;
 
-    nix-direnv = {
-      enable = true;
-      enableFlakes = true;
-    };
+    nix-direnv.enable = true;
   };
 }
