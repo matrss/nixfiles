1c19ab018d373f65d03a037ac95238ab0664e72c
Matthias Riße <matthias.risze@t-online.de>
2022-03-04 16:45:18 +0100

    Fix authelia configuration

--- profiles/oci-container/authelia/default.nix
+++ profiles/oci-container/authelia/default.nix
@@ -5,6 +5,7 @@
     authelia-jwt-secret = { };
     authelia-session-secret = { };
     authelia-notifier-smtp-password = { };
+    authelia-storage-encryption-key = { };
   };
 
   virtualisation.oci-containers.containers.authelia = {
@@ -14,12 +15,14 @@
       AUTHELIA_JWT_SECRET_FILE = "${config.sops.secrets.authelia-jwt-secret.path}";
       AUTHELIA_SESSION_SECRET_FILE = "${config.sops.secrets.authelia-session-secret.path}";
       AUTHELIA_NOTIFIER_SMTP_PASSWORD_FILE = "${config.sops.secrets.authelia-notifier-smtp-password.path}";
+      AUTHELIA_STORAGE_ENCRYPTION_KEY_FILE = "${config.sops.secrets.authelia-storage-encryption-key.path}";
     };
 
     volumes = [
       "${config.sops.secrets.authelia-jwt-secret.path}:${config.sops.secrets.authelia-jwt-secret.path}:ro"
       "${config.sops.secrets.authelia-session-secret.path}:${config.sops.secrets.authelia-session-secret.path}:ro"
       "${config.sops.secrets.authelia-notifier-smtp-password.path}:${config.sops.secrets.authelia-notifier-smtp-password.path}:ro"
+      "${config.sops.secrets.authelia-storage-encryption-key.path}:${config.sops.secrets.authelia-storage-encryption-key.path}:ro"
       "${./configuration.yml}:/configuration.yml:ro"
       "/volumes/authelia-config:/config:rw"
     ];
