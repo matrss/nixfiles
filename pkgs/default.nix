752b15318c41e4f59a2e0ab1b567330838f5cb73
Matthias Riße <matthias.risze@t-online.de>
2022-03-26 12:31:21 +0100

    Remove unused packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,4 @@
 final: prev: {
-  # datalad = prev.python37Packages.callPackage ./datalad.nix { };
-  # networkmanager-ssh = prev.callPackage ./networkmanager-ssh.nix { };
-
-  gitsh = prev.callPackage ./gitsh.nix { };
-  zk = prev.callPackage ./zk.nix { };
-
-  html-clock-plasmoid = prev.plasma5Packages.callPackage ./html-clock-plasmoid.nix { };
-
   scripts = prev.symlinkJoin {
     name = "scripts";
     paths = builtins.attrValues (import ./scripts.nix { pkgs = prev; });
