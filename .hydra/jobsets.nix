ca57c2a8e7d9ac56555f00bae91c3b7ea1e5fc81
Matthias Riße <matthias.risze@t-online.de>
2022-06-24 16:48:55 +0200

    Add declarative jobsets for GitLab MRs

--- .hydra/jobsets.nix
+++ .hydra/jobsets.nix
@@ -1,8 +1,24 @@
-{ nixpkgs, ... }:
+{ nixpkgs, pulls, ... }:
 
 let
   pkgs = import nixpkgs { };
-  jobsets = {
+  mrs = builtins.fromJSON (builtins.readFile pulls);
+  mrJobsetFor = mr: {
+    enabled = 1;
+    hidden = false;
+    description = "MR ${mr.iid}: ${mr.title}";
+    checkinterval = 60;
+    schedulingshares = 20;
+    enableemail = false;
+    emailoverride = "";
+    keepnr = 1;
+    type = 1;
+    flake = "gitlab:matrss/nixfiles/merge-requests/${mr.iid}/head";
+  };
+  mrJobsets = pkgs.lib.mapAttrs'
+    (name: value: pkgs.lib.nameValuePair ("MR" + name) (mrJobsetFor value))
+    mrs;
+  jobsets = mrJobsets // {
     main = {
       enabled = 1;
       hidden = false;
