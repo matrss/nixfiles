dc66d9f546c4fb6269dd87711ecb1d6959923b3d
Matthias Riße <matthias.risze@t-online.de>
2022-07-02 03:26:00 +0200

    Fix home-assistant configuration

--- hosts/nelvte/home-assistant.nix
+++ hosts/nelvte/home-assistant.nix
@@ -1,47 +1,53 @@
+{ config, ... }:
+
 {
-  services.home-assistant.enable = true;
-  services.home-assistant.config = {
-    homeassistant = {
-      name = "Home";
-      temperature_unit = "C";
-      unit_system = "metric";
-      time_zone = "Europe/Amsterdam";
-    };
-    frontend = { };
-    http = {
-      server_host = [ "127.0.0.1" ];
-      use_x_forwarded_for = true;
-      trusted_proxies = [ "127.0.0.1" ];
+  sops.secrets."home-assistant-secrets.yaml" = {
+    owner = config.users.users.hass.name;
+    path = "/var/lib/hass/secrets.yaml";
+  };
+
+  services.home-assistant = {
+    enable = true;
+    extraComponents = [
+      "default_config"
+      "met"
+      "esphome"
+    ];
+    config = {
+      homeassistant = {
+        external_url = "https://home.matrss.xyz";
+        name = "Home";
+        temperature_unit = "C";
+        unit_system = "metric";
+        time_zone = "Europe/Amsterdam";
+      };
+      frontend = { };
+      http = {
+        server_host = [ "127.0.0.1" ];
+        use_x_forwarded_for = true;
+        trusted_proxies = [ "127.0.0.1" ];
+      };
+      default_config = { };
+      met = { };
+      esphome = { };
+      zone = [
+        {
+          name = "Home";
+          latitude = "!secret latitude";
+          longitude = "!secret longitude";
+          radius = "!secret radius";
+          icon = "mdi:home";
+        }
+      ];
     };
   };
 
   services.nginx.virtualHosts."home.matrss.xyz" = {
     forceSSL = true;
     useACMEHost = "nelvte.matrss.xyz";
-    locations."/verify" = {
-      proxyPass = "http://127.0.0.1:9091/api/verify";
-      extraConfig = ''
-        internal;
-        proxy_pass_request_body off;
-        proxy_set_header Content-Length "";
-      '';
-    };
     locations."/" = {
       proxyPass = "http://127.0.0.1:8123";
       proxyWebsockets = true;
     };
-    extraConfig = ''
-      auth_request /verify;
-      auth_request_set $target_url $scheme://$http_host$request_uri;
-      auth_request_set $user $upstream_http_remote_user;
-      auth_request_set $groups $upstream_http_remote_groups;
-      auth_request_set $name $upstream_http_remote_name;
-      auth_request_set $email $upstream_http_remote_email;
-      proxy_set_header Remote-User $user;
-      proxy_set_header Remote-Groups $groups;
-      proxy_set_header Remote-Name $name;
-      proxy_set_header Remote-Email $email;
-      error_page 401 =302 https://idp.matrss.xyz/?rd=$target_url;
-    '';
   };
 }
