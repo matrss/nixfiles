2d51577c42b5413b74ccb33efcad408cf58ab30f
Matthias Riße <matthias.risze@t-online.de>
2023-01-19 20:40:20 +0100

    Update datalad

--- pkgs/datalad.nix
+++ pkgs/datalad.nix
@@ -1,5 +1,8 @@
 { buildPythonPackage
+, argcomplete
+, installShellFiles
 , fetchPypi
+, pythonRelaxDepsHook
 , distro
 , platformdirs
 , packaging
@@ -10,6 +13,7 @@
 , simplejson
 , keyrings-alt
 , PyGithub
+, looseversion
 , boto
 , keyring
 , jsmin
@@ -29,20 +33,26 @@
 , pillow
 , python-xmp-toolkit
 , pypandoc
-, pytest
+, psutil
+, pytestCheckHook
 , nose
 , git
 , openssh
+, p7zip
 }:
 
 buildPythonPackage rec {
   pname = "datalad";
-  version = "0.17.5";
+  version = "0.18.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-oiExLFiwubV2BcwaIoiDjySTJJGy5QR13XqUAVHK/M0=";
+    sha256 = "sha256-2mmYXfcNV6RJLqK3WH7nKPGq2q/+SfPwN5gWQzXF9Eg=";
   };
+
+  nativeBuildInputs = [ pythonRelaxDepsHook installShellFiles git ];
+
+  pythonRelaxDeps = [ "chardet" ];
 
   buildInputs = [ pypandoc ];
 
@@ -55,6 +65,8 @@ buildPythonPackage rec {
     exifread
     pillow
     python-xmp-toolkit
+    looseversion
+    psutil
     wrapt
     requests
     msgpack
@@ -75,19 +87,54 @@ buildPythonPackage rec {
     iso8601
     setuptools
     chardet
+    argcomplete
   ];
 
-  checkInputs = [ pytest nose git git-annex openssh ];
-
-  prePatch = ''
-    sed -i 's/chardet>=3.0.4, <5.0.0/chardet/g' setup.py
+  postInstall = ''
+    $out/bin/datalad shell-completion
+    installShellCompletion --cmd datalad \
+      --bash <($out/bin/datalad shell-completion) \
+      --zsh  <($out/bin/datalad shell-completion)
   '';
 
-  doCheck = false;
+  checkInputs = [ pytestCheckHook nose git git-annex openssh p7zip ];
 
-  checkPhase = ''
-    make bin
-    export PATH="$PWD/bin:$PATH"
-    python -m nose --traverse-namespace -s -v -e "test_cfg_override" -e "test_commanderror_jsonmsgs" -e "test_autoenabled_remote_msg" -e "test_clone_crcns" -e "test_clone_datasets_root" -e "test_gin_cloning" -e "test_ria_http_storedataladorg" -e "test_reckless" -e "test_clone_dataset_from_just_source" -e "test_clone_simple_local" -e "test_nonuniform_adjusted_subdataset" -e "test_create" -e "test_url_keys" -e "test_binary_data" -e "test_failed_install_multiple" -e "test_install_datasets_root" -e "test_install_recursive_github" -e "test_datasets_datalad_org" -e "test_install_dataset_from_just_source" -e "test_install_dataset_from_just_source_via_path" -e "test_install_simple_local" -e "test_remove_recursive_2" -e "test_deny_access" -e "test_add_archive_single_file" -e "test_download_url_need_datalad_remote" -e "test_shell_completion_source" -e "test_protocols" -e "test_compress_dir" -e "test_get_versioned_url_anon" -e "test_clone" -e "test_cached_dataset" -e "test_cached_url" -e "test_get_cached_dataset" -e "test__version__" -e "test_download_url_exceptions" datalad
+  preCheck = ''
+    export DATALAD_TESTS_NONETWORK=1
+    export HOME=$(mktemp -d)
+    export PATH="$out/bin:$PATH"
   '';
+
+  # Disable some very slow tests
+  pytestFlagsArray = [
+    "-m 'not turtle and not slow'"
+  ];
+
+  # These tests simply did not work in nixpkgs, not sure why exactly
+  disabledTests = [
+    "test_reckless"
+    "test_fetch_git_special_remote"
+    "test_create"
+    "test_initremote_basic_httpsurl"
+    "test_url_keys"
+    "test_addurls"
+    "test_drop_after"
+    "test_copy_file"
+    "test_AnnexRepo_web_remote"
+    "test_AnnexRepo_addurl_to_file_batched"
+    "test_annexrepo_fake_dates_disables_batched"
+    "test_as_common_datasource"
+    "test_shell_completion_source"
+    "test_add_archive_dirs"
+    "test_add_archive_content"
+    "test_add_archive_content_strip_leading"
+    "test_AnnexRepo_backend_option"
+    "test_is_available"
+    "test_AnnexRepo_addurl_batched_and_set_metadata"
+    "test_report_absent_keys"
+    "test_BasicAnnexTestRepo"
+    "test_get_open_files"
+  ];
+
+  pythonImportsCheck = [ "datalad" ];
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,13 @@
 final: prev:
 let
-  dataladFor = pythonPackages: rec {
-    datalad = pythonPackages.callPackage ./datalad.nix { };
+  pythonPackagesFor = pythonPackages: rec {
+    datalad = pythonPackages.callPackage ./datalad.nix { inherit looseversion; };
     datalad-container = pythonPackages.callPackage ./datalad-container.nix { inherit datalad; };
+    looseversion = pythonPackages.callPackage ./looseversion.nix { };
   };
 in
 {
-  python39Packages = prev.python39Packages // (dataladFor final.python39Packages);
+  python39Packages = prev.python39Packages // (pythonPackagesFor prev.python39Packages);
 
   datalad = final.callPackage
     ({ lib, pythonPackages, extraPackages ? [ ] }: with pythonPackages; (toPythonApplication datalad).overrideAttrs (_: {
@@ -14,7 +15,12 @@ in
         wrapProgram $out/bin/datalad --suffix PYTHONPATH : ${lib.makeSearchPath python.sitePackages (datalad.requiredPythonModules ++ extraPackages ++ (builtins.concatMap (p: p.requiredPythonModules) extraPackages))}:$out/${python.sitePackages}
       '';
     }))
-    { pythonPackages = final.python39Packages; extraPackages = [ final.python39Packages.datalad-container ]; };
+    {
+      pythonPackages = final.python39Packages;
+      extraPackages = [
+        final.python39Packages.datalad-container
+      ];
+    };
 
   generate-hostnames = prev.callPackage (import ./scripts.nix).generate-hostnames { };
 }
