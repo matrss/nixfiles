5dae89e942760b24bdbf0329f56c025cc35a63df
Matthias Riße <matthias.risze@t-online.de>
2023-02-17 23:15:24 +0100

    Add offsite backups

--- hosts/nelvte/restic.nix
+++ hosts/nelvte/restic.nix
@@ -1,39 +1,48 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   sops.secrets."restic/repository-password" = { };
 
   services.restic.backups.local-backup = {
     repository = "/backup/restic-repo";
-    paths = [ "/data/snapshots/mpanra-varlib" ];
+    paths = [ "/data/snapshots/mpanra-varlib-local" ];
     passwordFile = config.sops.secrets."restic/repository-password".path;
-  };
-
-  systemd.services.before-restic-backups-local-backup = {
-    description = "Prepare system for backup";
-    partOf = [ "restic-backups-local-backup.service" ];
-    wantedBy = [ "restic-backups-local-backup.service" ];
-    before = [ "restic-backups-local-backup.service" ];
-    path = [ "/run/current-system/sw" ];
-    serviceConfig.Type = "oneshot";
-    script = ''
+    timerConfig = {
+      OnCalendar = "01:00";
+    };
+    backupPrepareCommand = ''
       systemctl stop container@mpanra.service
       sleep 5
-      btrfs subvolume snapshot -r /data/mpanra-varlib /data/snapshots/mpanra-varlib
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume snapshot -r /data/mpanra-varlib /data/snapshots/mpanra-varlib-local
       sleep 5
       systemctl start container@mpanra.service
     '';
+    backupCleanupCommand = ''
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume delete /data/snapshots/mpanra-varlib-local
+    '';
   };
 
-  systemd.services.after-restic-backups-local-backup = {
-    description = "Cleanup system after backup";
-    partOf = [ "restic-backups-local-backup.service" ];
-    wantedBy = [ "restic-backups-local-backup.service" ];
-    after = [ "restic-backups-local-backup.service" ];
-    path = [ "/run/current-system/sw" ];
-    serviceConfig.Type = "oneshot";
-    script = ''
-      btrfs subvolume delete /data/snapshots/mpanra-varlib
+  sops.secrets."restic/ssh-keys/backup-box" = { };
+
+  services.restic.backups.offsite-backup = {
+    extraOptions = [
+      "sftp.command='ssh u338890@u338890.your-storagebox.de -i ${config.sops.secrets."restic/ssh-keys/backup-box".path} -s sftp'"
+    ];
+    repository = "sftp:u338890@u338890.your-storagebox.de:backups/mpanra-varlib";
+    paths = [ "/data/snapshots/mpanra-varlib-offsite" ];
+    passwordFile = config.sops.secrets."restic/repository-password".path;
+    timerConfig = {
+      OnCalendar = "02:00";
+    };
+    backupPrepareCommand = ''
+      systemctl stop container@mpanra.service
+      sleep 5
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume snapshot -r /data/mpanra-varlib /data/snapshots/mpanra-varlib-offsite
+      sleep 5
+      systemctl start container@mpanra.service
+    '';
+    backupCleanupCommand = ''
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume delete /data/snapshots/mpanra-varlib-offsite
     '';
   };
 }
