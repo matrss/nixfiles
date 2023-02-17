677bd574efc42cffaf01ef03c2aa752415a2c0fa
Matthias Riße <matthias.risze@t-online.de>
2023-02-17 20:26:26 +0100

    Setup backups of mpanra service data

--- hosts/nelvte/restic.nix
+++ hosts/nelvte/restic.nix
@@ -5,7 +5,33 @@
 
   services.restic.backups.local-backup = {
     repository = "/backup/restic-repo";
-    paths = [ ];
+    paths = [ "/data/snapshots/mpanra-varlib" ];
     passwordFile = config.sops.secrets."restic/repository-password".path;
+  };
+
+  systemd.services.before-restic-backups-local-backup = {
+    description = "Prepare system for backup";
+    partOf = [ "restic-backups-local-backup.service" ];
+    wantedBy = [ "restic-backups-local-backup.service" ];
+    before = [ "restic-backups-local-backup.service" ];
+    path = [ "/run/current-system/sw" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      systemctl stop container@mpanra.service
+      btrfs subvolume snapshot -r /data/mpanra-varlib /data/snapshots/mpanra-varlib
+      systemctl start container@mpanra.service
+    '';
+  };
+
+  systemd.services.after-restic-backups-local-backup = {
+    description = "Cleanup system after backup";
+    partOf = [ "restic-backups-local-backup.service" ];
+    wantedBy = [ "restic-backups-local-backup.service" ];
+    after = [ "restic-backups-local-backup.service" ];
+    path = [ "/run/current-system/sw" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      btrfs subvolume delete /data/snapshots/mpanra-varlib
+    '';
   };
 }
