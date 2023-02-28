f8a160aef0cfe18921cfc51b51dcdaa9a2f02ec6
Matthias Riße <matthias.risze@t-online.de>
2023-02-28 19:00:29 +0100

    Migrate scripts to writeShellApplication

--- pkgs/scripts.nix
+++ pkgs/scripts.nix
@@ -1,19 +1,27 @@
 {
-  dply = { writeShellScriptBin, nixos-rebuild }: writeShellScriptBin "dply" ''
-    ${nixos-rebuild}/bin/nixos-rebuild switch --flake .#"$1" \
-      --target-host root@"$1".m.0px.xyz \
-      --build-host root@"$1".m.0px.xyz
-  '';
+  dply = { writeShellApplication, nixos-rebuild }: writeShellApplication {
+    name = "dply";
+    runtimeInputs = [ nixos-rebuild ];
+    text = ''
+      nixos-rebuild switch --flake .#"$1" \
+        --target-host root@"$1".m.0px.xyz \
+        --build-host root@"$1".m.0px.xyz
+    '';
+  };
 
-  generate-hostnames = { writeShellScriptBin, python3 }: writeShellScriptBin "generate-hostnames" ''
-    ${python3}/bin/python3 << EOF
-    import string
-    import random
+  generate-hostnames = { writeShellApplication, python3 }: writeShellApplication {
+    name = "generate-hostnames";
+    runtimeInputs = [ python3 ];
+    text = ''
+      python3 << EOF
+      import string
+      import random
 
-    columns = 5
+      columns = 5
 
-    for i in range(100):
-        print("".join(random.choices(string.ascii_lowercase, k=6)), end="\n" if i % columns == (columns-1) else "\t")
-    EOF
-  '';
+      for i in range(100):
+          print("".join(random.choices(string.ascii_lowercase, k=6)), end="\n" if i % columns == (columns-1) else "\t")
+      EOF
+    '';
+  };
 }
