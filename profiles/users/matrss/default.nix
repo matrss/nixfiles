ad4e892289eb9b58adfaf9bb5b2f06d1d6a5720d
Matthias Riße <matthias.risze@t-online.de>
2023-05-18 01:11:14 +0200

    Make vscodium less declarative

--- profiles/users/matrss/default.nix
+++ profiles/users/matrss/default.nix
@@ -18,6 +18,8 @@
       bitwarden-cli
       autossh
       nodePackages.tiddlywiki
+      vscodium-fhs
+      nil
     ];
 
     home.homeDirectory = "/home/matrss";
@@ -62,55 +64,6 @@
     programs.tmux.baseIndex = 1;
     programs.tmux.clock24 = true;
     programs.tmux.newSession = true;
-
-    programs.vscode.enable = true;
-    programs.vscode.package = pkgs.vscodium;
-    programs.vscode.extensions = with pkgs.vscode-extensions; [
-      asvetliakov.vscode-neovim
-      streetsidesoftware.code-spell-checker
-      ms-vscode-remote.remote-ssh
-      mkhl.direnv
-      editorconfig.editorconfig
-      donjayamanne.githistory
-      alefragnani.project-manager
-      james-yu.latex-workshop
-      bbenoist.nix
-      ms-python.python
-      rust-lang.rust-analyzer
-      tamasfe.even-better-toml
-      vadimcn.vscode-lldb
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "signageos-vscode-sops";
-        publisher = "signageos";
-        version = "0.6.1";
-        sha256 = "sha256-fHXiIfc+EXGzR1nl9x87nyKVvLGS1zW6hV5D0CxvUCg=";
-      }
-      {
-        name = "base16-themes";
-        publisher = "AndrsDC";
-        version = "1.4.5";
-        sha256 = "sha256-molx+cRKSB6os7pDr0U1v/Qbaklps+OvBkZCkSWEvWM=";
-      }
-      {
-        name = "code-spell-checker-german";
-        publisher = "streetsidesoftware";
-        version = "2.1.0";
-        sha256 = "sha256-9IX7Vl7wqeDyJ5f6+HVpndfmIOQadvRmtogvNLfGCvw=";
-      }
-      {
-        name = "code-spell-checker-scientific-terms";
-        publisher = "streetsidesoftware";
-        version = "0.1.6";
-        sha256 = "sha256-g6GD8ie9E5SmImxe9hBQe8ZSF+nKQMlZzoK7dm2NIW4=";
-      }
-      {
-        name = "excalidraw-editor";
-        publisher = "pomdtr";
-        version = "3.3.4";
-        sha256 = "sha256-a3WECVmyz1fTAnK1SApsNlLKNVq/BPC0t3fArN2P/60=";
-      }
-    ];
 
     home.stateVersion = "20.09";
   };
