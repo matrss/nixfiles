14f48b6f1339c605432cdfb7cc18b777c43ba5be
Matthias Riße <matthias.risze@t-online.de>
2023-05-29 21:47:47 +0200

    Fix failing nix daemon connection in buildbot

--- hosts/nelvte/configuration.nix
+++ hosts/nelvte/configuration.nix
@@ -9,6 +9,9 @@
     ./restic.nix
   ];
 
+  # Required to make buildbot work, since it is using nelvte's nix daemon
+  nix.settings.allowed-users = [ "*" ];
+
   networking.hostName = "nelvte";
   networking.domain = "m.0px.xyz";
 
