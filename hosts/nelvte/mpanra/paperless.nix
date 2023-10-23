576195f505cd6fe9d18b225f1057c6956edd5a63
Matthias Riße <matthias.risze@t-online.de>
2023-10-23 22:41:05 +0200

    Fix ghostscript error on some documents

--- hosts/nelvte/mpanra/paperless.nix
+++ hosts/nelvte/mpanra/paperless.nix
@@ -8,6 +8,8 @@
   services.paperless.extraConfig = {
     PAPERLESS_URL = "https://paperless.0px.xyz";
     PAPERLESS_OCR_LANGUAGE = "deu+eng";
+    # Required for some documents, see: https://docs.paperless-ngx.com/troubleshooting/#consumption-fails-with-ghostscript-pdfa-rendering-failed
+    PAPERLESS_OCR_USER_ARGS = "{\"continue_on_soft_render_error\": true}";
   };
 
   systemd.services.paperless-web.serviceConfig.EnvironmentFile = config.sops.secrets."paperless/secret-env".path;
