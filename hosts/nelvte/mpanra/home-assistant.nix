37d9598da7c7ec60ff009cbeb52a16408338f0f4
Matthias Riße <matthias.risze@t-online.de>
2023-10-27 22:15:46 +0200

    Enable brute force protection on home-assistant

--- hosts/nelvte/mpanra/home-assistant.nix
+++ hosts/nelvte/mpanra/home-assistant.nix
@@ -26,6 +26,8 @@
         server_host = [ "127.0.0.1" ];
         use_x_forwarded_for = true;
         trusted_proxies = [ "127.0.0.1" ];
+        ip_ban_enabled = true;
+        login_attempts_threshold = 5;
       };
       default_config = { };
       met = { };
