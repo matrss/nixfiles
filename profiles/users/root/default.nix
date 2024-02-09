e05669237d1bc37d7c7091a32469430ae3f6116a
Matthias Riße <matthias.risze@t-online.de>
2024-02-09 18:28:28 +0100

    Set up automatic deployments from main branch

--- profiles/users/root/default.nix
+++ profiles/users/root/default.nix
@@ -1,5 +1,6 @@
 {
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIByQhPnALCgo9Q4FbqYBCSTbMbP6OuSNmgRafdDo6yAx matrss@ipsmin"
+    "command=\"system-upgrade\" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAxL0AN/OIueekEBUTvjdJippFSHP0gzooz66rvmLFL GitHub Actions deploy"
   ];
 }
