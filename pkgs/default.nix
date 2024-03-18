bff7487fef392314722989fc0c69c400286953c9
Matthias Riße <matthias.risze@t-online.de>
2024-03-18 21:48:39 +0100

    Remove obsolete terraform deployment code

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,6 @@
 _final: prev:
 
 {
-  dply = prev.callPackage (import ./scripts.nix).dply { };
   generate-hostnames = prev.callPackage (import ./scripts.nix).generate-hostnames { };
-  nixos-config-path = prev.callPackage (import ./scripts.nix).nixos-config-path { };
   smtp2paperless = prev.callPackage ./smtp2paperless.nix { };
 }
--- pkgs/scripts.nix
+++ pkgs/scripts.nix
@@ -1,21 +1,4 @@
 {
-  dply = { writeShellApplication, nixos-rebuild }: writeShellApplication {
-    name = "dply";
-    runtimeInputs = [ nixos-rebuild ];
-    text = ''
-      nixos-rebuild switch --flake .#"$1" --target-host "root@$1" --use-substitutes
-    '';
-  };
-
-  nixos-config-path = { writeShellApplication, jq }: writeShellApplication {
-    name = "nixos-config-path";
-    runtimeInputs = [ jq ];
-    text = ''
-      nix path-info --json .#nixosConfigurations.\""$1"\".config.system.build.toplevel |
-        jq 'to_entries | { "path": .[0].key }'
-    '';
-  };
-
   generate-hostnames = { writeShellApplication, python3 }: writeShellApplication {
     name = "generate-hostnames";
     runtimeInputs = [ python3 ];
